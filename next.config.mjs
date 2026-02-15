552780ab851107c5632c3af166e7ce6f9bf780a2
gambolboy4444-ops <gambolboy4444@gmail.com>
2026-02-16 05:00:27 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,3 @@
-git add .
-git commit -m "COMPLETE: ts deleted, mjs created. THE END."
-git push origin main --force
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+export default nextConfig;
